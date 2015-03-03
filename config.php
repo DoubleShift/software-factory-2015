d2aadae90845fa6cb95d88b658f713749f7d27eb
Double <fjjwlzd@163.com>
2015-03-03 11:08:44 +0200

    go back, config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+File: config.php
+Description: Main config file of the project. You must include it to all your files using php inlcude() function
+Author: Alexander Samodelkin
+Version: 0.1
+Created: 13.02.2015
+*/
+
+    define( 'IP_ROOT', 'http://sfp.psicode.ru/' );
+	define( 'IP_INTERFACE', IP_ROOT . 'interface/' );
+	define( 'IP_CSS', IP_INTERFACE . 'styles/' );
+	define( 'IP_PICTURES', IP_INTERFACE . 'pictures/' );
+	define( 'IP_SCRIPTS', IP_INTERFACE . 'scripts/' );
+	
+	define( 'P_INTERFACE', P_ROOT . 'interface/' );
+	define( 'P_LANGUAGE', P_INTERFACE . 'mui/' ); 
+
+	define( 'SQL_HOST', 'localhost' );
+	define( 'SQL_DB', 'psicoderu_sfp' );
+    define( 'SQL_USER', 'psicoderu_sfp' );
+    define( 'SQL_PASS', 'sfp12345' );
+
+	define( 'DEBUG', '0' );
+?>
