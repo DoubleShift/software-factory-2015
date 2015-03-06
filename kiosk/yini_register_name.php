7bcd355b1e6fbdf1d669c096ddc9630c3bab9645
yini <leagillrena@hotmail.com>
2015-03-06 22:57:17 +0200

    yini

--- kiosk/yini_register_name.php
+++ kiosk/yini_register_name.php
@@ -1,21 +1,36 @@
 <?php
 
 /*
-File: report.php
-Description: report a problem.
-Author: Chen Xin
+File: register_name.php
+Description: register name
+Author: Yini Wang
 Version: 0.1
-Created: 24.02.2015
+Created: 6.3.2015
 */
 
 	// Include header
 	include( dirname(__FILE__) . '/header.php' );
 
-
 ?>
 
-<script>
+<div class="wrapper">
+	<div class="content register-name">
+		<div class="heading">
+			<h1>Enter your name</h1>
+		</div>
+		<div class="body">
+			<p>This will be hte name for your registration that is assigned with your card.</p>
+			<form action="post" class="form register register-name">
+				<input type="text" id="input_name" />
+				<button class="btn next" onclick="btn_next_onclick()">Next<span></span></button>
+			</form>
+		</div>
+	</div>
+	<div class="keyboard"></div>
+</div>
 
+<script type="text/javascript">
+	
 function btn_next_onclick(){
 	var user_name=document.getElementById("input_name").value;
 	setCookie("username", user_name);   
@@ -24,21 +39,6 @@ function btn_next_onclick(){
 
 </script>
 
-	<div class="wrapper">
-		<div class="content register name">
-			<div class="heading">
-				<h1>Enter your name</h1>
-			</div>
-			<div class="body">
-				<p>This will be hte name for your registration that is assigned with your card.</p>
-				<form action="post" class="form">
-					<input type="text" id="input_name" />
-					<button class="btn next" onclick="btn_next_onclick()">Next<span></span></button>
-				</form>
-			</div>
-		</div>
-		<div class="keyboard"></div>
-	</div>
 
 
 <?php /* CLOSE THIS TAGS THAT WERE OPENED IN HEADER */ ?>
