4435eedda14643bc50e3adda53872d6b653edab7
yini <leagillrena@hotmail.com>
2015-03-07 01:03:40 +0200

    no message

--- kiosk/javascript.js
+++ kiosk/javascript.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
 	//On register pages, select of info block
-	$('body').on('.register-block','click',function(e){
-
-		e.preventDefault();
+	$('body').on('.register-block', 'click', function(e){
 
 		if($(this).hasClass('selected')){
 			$(this).removeClass('selected').find('input').removeAttr('checked');
 		}else{
 			$(this).addClass('selected').find('input').attr('checked','checked');
 		}
+
+		return false;
 		
 	});
 
