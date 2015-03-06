6973d94deb98c8d90005d15f5246b5209138fe9e
yini <leagillrena@hotmail.com>
2015-03-07 00:52:09 +0200

    yini

--- kiosk/javascript.js
+++ kiosk/javascript.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
 	//On register pages, select of info block
-	$('body').on('.register-block','click',function(){
+	$('body').on('.register-block','click',function(e){
+
+		e.preventDefault();
 
 		if($(this).hasClass('selected')){
 			$(this).removeClass('selected').find('input').removeAttr('checked');
