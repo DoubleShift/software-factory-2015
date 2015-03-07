e2de72668c646f6d127f54b25a5655874da941b4
chinapumpkin <1278917737@qq.com>
2015-03-07 16:15:23 +0200

    for test

--- kiosk/dcr_error.php
+++ kiosk/dcr_error.php
@@ -20,7 +20,8 @@ changed: 07.03.2015
 	<div id="error-image"></div>
 	<div id="error-text"><?=Ooops! Card can not be found?></div>
 	<hr align=right width=430 color=#000000 SIZE=3 noShade> 
-	<div id="welcome-text"><a href="welcome.php" ><?=ERROR_TIPS?></a></div>
+	<!---<div id="welcome-text">
+		<a href="welcome.php" ><?=ERROR_TIPS?></a></div> -->
 
 </div>
 	
