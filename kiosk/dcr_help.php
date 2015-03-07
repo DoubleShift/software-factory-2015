f100d1b7a630c514ddb09c9b6e87238495b6a781
chinapumpkin <1278917737@qq.com>
2015-03-07 17:43:40 +0200

    no message

--- kiosk/dcr_help.php
+++ kiosk/dcr_help.php
@@ -13,6 +13,18 @@ Created: 15.02.2015
 
 	/* PAGE CODE STARTS AFTER THIS SECTION */ 
 ?>
+<script>
+/* waiting to write*/
+function btn_next_onclick(){
+
+	/*var user_problem=document.getElementById("input_problem").value;
+	setCookie("userproblem", user_problem);   
+	location.href='yini_register_email.php';*/
+
+	
+}
+
+</script>
 
 <div class="wrapper">
 	<div class="content register-name">
@@ -23,13 +35,13 @@ Created: 15.02.2015
 			<h1>Help</h1>
 		</div>
 
-	<div class="body" style="width:100%;">
+	<div class="body" >
 	
 	<h2> Select the topic you need help from </h2>
 	
 	<table  width="100%" cellpadding="5px" cellspacing="20px">
 	<tr>
-    <td colspan="2" height="30" width="100%"><a href="#" class="help_option">
+    <td colspan="2" height="30" ><a href="#" class="help_option">
         How to use our system
 	</a> </td>
     
@@ -37,7 +49,7 @@ Created: 15.02.2015
 	<tr>
 	</tr>
 	<tr>
-    <td colspan="2" height="30" width="100%">
+    <td colspan="2" height="30" >
 	<a href="#" class="help_option">
        What is the standard of star level evaluation 
 	</a> 
@@ -52,6 +64,14 @@ Created: 15.02.2015
         How can I get contacted with my Facebook Account
 	</a>  </td>
 	</tr>
+	<tr>
+						<td>
+							<button class="btn prev" onclick="javascript:history.go(-1);">Back</button>
+						</td>
+						<td align="right">
+							<button class="btn next" onclick="btn_next_onclick()">Next</button>
+						</td>
+					</tr>
 	</table>
 	
    </div>
