efad7500aab426b77e9fdc463dad336649b6cca4
chinapumpkin <1278917737@qq.com>
2015-03-07 18:11:17 +0200

    no message

--- kiosk/dcr_report.php
+++ kiosk/dcr_report.php
@@ -13,10 +13,42 @@ Created: 24.02.2015
 
 	/* PAGE CODE STARTS AFTER THIS SECTION */ 
 ?>
+<script>
+function btn_next_onclick(){
 
+	/*var user_problem=document.getElementById("input_problem").value;
+	setCookie("userproblem", user_problem);   
+	location.href='yini_register_email.php';*/
+
+	
+}
+</script>
 <div id="welcome-picture"></div>
-
-
-
+<div class="wrapper">
+	<div class="content register-name">
+	<div class="heading">
+			<h1>Report a problem</h1>
+			
+		</div>
+	<div class="body">
+	 <h2>Select the problematic equipment</h2>
+	</div>
+	<table >
+	<tr>
+	<h2>Describe the problem</h2>
+	</tr>
+	<tr>
+	<textarea style="width:100%;" id="register_other_problem"></textarea>
+	<tr>
+						<td>
+							<button class="btn prev" onclick="JavaScript:history.go(-1);">Back</button>
+						</td>
+						<td align="right">
+							<button class="btn next" onclick="btn_next_onclick()">Next</button>
+						</td>
+					</tr>
+	</table>
+</div>
+</div>
 <?php /* CLOSE THIS TAGS THAT WERE OPENED IN HEADER */ ?>
 </body></html>
