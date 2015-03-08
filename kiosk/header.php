ae78513e010a9412112e0661dbbf3d8dd9cb0f20
yini <leagillrena@hotmail.com>
2015-03-08 15:32:35 +0200

    yini header.php

--- kiosk/header.php
+++ kiosk/header.php
@@ -53,7 +53,7 @@ Created: 13.02.2015
 <table id="header" cellpadding="0" cellspacing="0" border="0">
 <tr valign="middle">
 	<td width="50">&nbsp;</td>
-	<td><a class="logo" href="welcome.php"><img src="../interface/pictures/logo.png" alt="Health Ex" /></a></td>
+	<td width="180"><a class="logo" href="welcome.php"><img src="../interface/pictures/logo.png" alt="Health Ex" /></a></td>
 	<!--<td id="header-text"><span class="green">H</span>ealth<span class="green">E</span>x</td>-->
 	<td width="400">
 		<img class="language<?php if($_SESSION['language']!='fi') echo ' opacity' ?>" src="<?=IP_PICTURES;?>fi.png" onclick="location.href='<?php echo curPage();?>?lang=fi'" />
